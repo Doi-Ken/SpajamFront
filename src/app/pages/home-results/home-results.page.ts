e0ae46d103d54a9e74752212ee799c39ea9779e2
土井健 <doiken@doikennoMacBook-Pro.local>
2019-06-23 11:22:56 +0900

    calories呼び出し

--- src/app/pages/home-results/home-results.page.ts
+++ src/app/pages/home-results/home-results.page.ts
@@ -6,6 +6,8 @@ import {
   ToastController,
   PopoverController,
   ModalController } from '@ionic/angular';
+import { OnInit } from '@angular/core';
+import { HttpService } from '../../services/http/httpservice.service';
 
 // Modals
 import { SearchFilterPage } from '../../pages/modal/search-filter/search-filter.page';
@@ -14,20 +16,24 @@ import { ImagePage } from './../modal/image/image.page';
 import { NotificationsComponent } from './../../components/notifications/notifications.component';
 import * as Chart from 'chart.js';
 
+
 @Component({
   selector: 'app-home-results',
   templateUrl: './home-results.page.html',
   styleUrls: ['./home-results.page.scss']
 })
 
-export class HomeResultsPage {
+export class HomeResultsPage implements OnInit {
   searchKey = '';
   weight_goal = 'XX.X';
-  today_intake = 'YYYY';
-  today_consume = 'ZZZZ';
-  today_total= 'WWWW';
+  public today_intake;
+  public today_consume;
+  public today_total;
   weightChart: Chart;
   themeCover = 'assets/img/ionic4-Start-Theme-cover.jpg';
+
+  calorie_url = "http://ec2-13-115-231-20.ap-northeast-1.compute.amazonaws.com/calories";
+  intake_url = "http://ec2-13-115-231-20.ap-northeast-1.compute.amazonaws.com/intake";
 
   @ViewChild('chartContainer') chartcontainer: ElementRef;
   @ViewChild('chartcanvas') chartcanvas: ElementRef;
@@ -36,19 +42,41 @@ export class HomeResultsPage {
     this.createWeightChart();
   }
 
+  ngOnInit() {
+    this.get();
+    console.log(this.today_consume);
+    console.log(this.today_intake);
+  }
+
   constructor(
     public navCtrl: NavController,
     public menuCtrl: MenuController,
     public popoverCtrl: PopoverController,
     public alertCtrl: AlertController,
     public modalCtrl: ModalController,
-    public toastCtrl: ToastController
+    public toastCtrl: ToastController,
+    public http: HttpService
   ) {
 
   }
 
+  async get() {
+    return this.http.get(this.calorie_url)
+    .then(res => { this.today_intake = res['intake'];
+                    this.today_consume = res['consume'];
+                    this.today_total = this.today_intake - this.today_consume;
+                    console.log(res);
+    });
+  }
+
   ionViewWillEnter() {
     this.menuCtrl.enable(true);
+  }
+
+  notifications(){
+    this.get();
+    console.log(this.today_consume);
+    console.log(this.today_intake);
   }
 
   createWeightChart(){
--- src/app/services/http/httpservice.service.ts
+++ src/app/services/http/httpservice.service.ts
@@ -7,35 +7,37 @@ import { Observable } from "rxjs";
 @Injectable()
 export class HttpService {
     
-  constructor(
-    private http: Http
+    constructor(
+      private http: Http
   ){}
+
+  
 
   // 取得
   async get(url: string): Promise<any[]>{
-    return this.http.get(url).toPromise()
-    .then(this.extractData)
-    .catch(this.handlerError);
-  }
-
-  // 登録
-  async post(url: string, jsondata: string): Promise<any[]> {
-    let headers = new Headers({'Content-Type': 'application/json'});
-    let options = new RequestOptions({ headers: headers });
-
-    return this.http.post(url, jsondata).toPromise()
+      return this.http.get(url).toPromise()
       .then(this.extractData)
       .catch(this.handlerError);
   }
 
+  // 登録
+  async post(url: string, jsondata: string): Promise<any[]> {
+      let headers = new Headers({'Content-Type': 'application/json'});
+      let options = new RequestOptions({ headers: headers });
+
+      return this.http.post(url, jsondata).toPromise()
+              .then(this.extractData)
+              .catch(this.handlerError);
+  }
+
   private extractData(res: Response){
-    let body = res.json();
-    return body || {};
+      let body = res.json();
+      return body || {};
   }
 
   private handlerError(error: Response | any){
-    console.log(error.toString());
-    return Observable.throw(error.toString());
+      console.log(error.toString());
+      return Observable.throw(error.toString());
   }
 
   
